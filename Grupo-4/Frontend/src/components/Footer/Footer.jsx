3d335a504ea9bb60407ca8b7e6dc23e4be867d2a
Juan de la Morena <delamorenajuan@gmail.com>
2024-05-09 15:12:40 +0200

    footer cuasi fisxed

--- Grupo-4/Frontend/src/components/Footer/Footer.jsx
+++ Grupo-4/Frontend/src/components/Footer/Footer.jsx
@@ -2,11 +2,8 @@ import "./Footer.module.css";
 
 export function Footer  ()  {
   return (
-    <footer className="footer">
-      <p>(C) 2024 - AhorraS</p>
-      <link rel="stylesheet" href="" />
-      <link rel="stylesheet" href="" />
-      <link rel="stylesheet" href="" />
+    <footer className="footer"><nav></nav>
+      <p>(C) 2024 - FinanSage</p>
     </footer>
   );
 }
