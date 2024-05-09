2da42b6f374b4a2326694e023e4f7db2a7c225f0
Daniel Pardo Martin <daniel.pardo.martin20@gmail.com>
2024-05-09 15:29:13 +0200

    llamada a la api

--- Grupo-4/Frontend/src/components/Call/Callairtable.jsx
+++ Grupo-4/Frontend/src/components/Call/Callairtable.jsx
@@ -1,23 +1,22 @@
-
 import { useEffect, useState } from 'react';
 import { fetchData } from '../services/airtable';
 
-const  Callairtable = () => {
-  const [records, setRecords] = useState([]);
+const Callairtable = () => {
+    const [records, setRecords] = useState([]);
 
-  useEffect(() => {
-    fetchData().then(data => setRecords(data));
-  }, []);
+    useEffect(() => {
+        fetchData().then((data) => setRecords(data));
+    }, []);
 
-  return (
-    <div>
-      {records.map(record => (
-        <div key={record.id}>
-          <p>{record.fields.Name}</p> 
+    return (
+        <div>
+            {records.map((record) => (
+                <div key={record.id}>
+                    <p>{record.fields.Name}</p>
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 };
 
 export default Callairtable;
