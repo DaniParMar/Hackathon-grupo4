6de2e1fe57a5c6d814f5cf1a16348062da843632
Juan de la Morena <delamorenajuan@gmail.com>
2024-05-09 11:25:28 +0200

    service y footer

---
+++ Grupo-4/src/components/Call/Callairtable.jsx
@@ -0,0 +1,23 @@
+
+import { useEffect, useState } from 'react';
+import { fetchData } from '../services/airtable';
+
+const  Callairtable = () => {
+  const [records, setRecords] = useState([]);
+
+  useEffect(() => {
+    fetchData().then(data => setRecords(data));
+  }, []);
+
+  return (
+    <div>
+      {records.map(record => (
+        <div key={record.id}>
+          <p>{record.fields.Name}</p> 
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Callairtable;
